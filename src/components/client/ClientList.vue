8ffbb356ccc15fda5f71ed73236b29b73d5840d7
hemlk <hiep@elitetech.vn>
2017-12-11 18:09:45 +0700

    code clean-up

--- src/components/client/ClientList.vue
+++ src/components/client/ClientList.vue
@@ -1,11 +1,40 @@
 <template>
-  <q-data-table ref="grdClient" class="full-height" :data="data" :config="config" :columns="columns" @refresh="getAllClients">
+  <q-data-table class="full-height" :data="data" :config="config" :columns="columns" @refresh="refresh">
+    <!-- Custom renderer when user selected one or more rows -->
+    <span slot="selection" slot-scope="selection">
+      <q-btn color="primary" @click="changeMessage(selection)">
+        <i>edit</i>
+      </q-btn>
+      <q-btn color="negative" @click="deleteRow(selection)">
+        <i>delete</i>
+      </q-btn>
+    </span>
   </q-data-table>
 </template>
 <script>
+import mxGrid from '../_mixins/Grid'
 export default {
+  mixins: [mxGrid],
+  data() {
+    return {
+      config: {
+        title: '<span class="text-negative"><b>Clients Information</b></span>',
+      },
+      columns: [
+        {label: 'Code', field: 'code', width: '75px', filter: true, sort: true, type: 'string'},
+        {label: 'Name', field: 'name', filter: true, sort: true, type: 'string'},
+        {label: 'Tax Code', field: 'tax_code', width: '100px', filter: true, sort: true, type: 'string'},
+        {label: 'Address', field: 'invoice_addr', filter: true, sort: true, type: 'string'},
+        {label: 'Delivery', field: 'delivery_addr', filter: true, sort: true, type: 'string'},
+        {label: 'Tel', field: 'tel', width: '120px', filter: true, sort: true, type: 'string'},
+        {label: 'Fax', field: 'fax', width: '120px', filter: true, sort: true, type: 'string'},
+      ],
+    }
+  },
   methods: {
-    getAllClients() {
+    changeMessage() {},
+    deleteRow() {},
+    refresh(done) {
       this.$http
         .get('/api', {
           params: {
@@ -23,57 +52,14 @@ export default {
           },
         })
         .then(({data}) => {
-          this.$refs.grdClient.refresh(false)
           this.data = data.getAllClients
+          done()
+        })
+        .catch(err => {
+          alert(err)
+          done()
         })
     },
-  },
-  data() {
-    return {
-      data: [],
-      config: {
-        rowHeight: '38px',
-        title: '<span class="text-negative"><b>Clients List</b></span>',
-        noHeader: false,
-        refresh: true,
-        columnPicker: true,
-        // Height auto-resizing
-        bodyStyle: {
-          height: 'calc(100vh - 272px)',
-        },
-        responsive: true,
-        pagination: {
-          rowsPerPage: 15,
-          options: [5, 10, 15, 30, 50, 500],
-        },
-        selection: 'multiple',
-        messages: {
-          noData: '<i>warning</i> No data available to show.',
-          noDataAfterFiltering: 'No results. Please refine your search terms.',
-        },
-        labels: {
-          columns: 'Columns',
-          allCols: 'All Fields',
-          rows: 'Rows',
-          selected: {
-            singular: 'Client selected.',
-            plural: 'Clients selected.',
-          },
-          clear: 'clear',
-          search: 'Search',
-          all: 'All',
-        },
-      },
-      columns: [
-        {label: 'Code', field: 'code', width: '75px', filter: true, sort: true, type: 'string'},
-        {label: 'Name', field: 'name', filter: true, sort: true, type: 'string'},
-        {label: 'Tax Code', field: 'tax_code', width: '100px', filter: true, sort: true, type: 'string'},
-        {label: 'Address', field: 'invoice_addr', filter: true, sort: true, type: 'string'},
-        {label: 'Delivery', field: 'delivery_addr', filter: true, sort: true, type: 'string'},
-        {label: 'Tel', field: 'tel', width: '120px', filter: true, sort: true, type: 'string'},
-        {label: 'Fax', field: 'fax', width: '120px', filter: true, sort: true, type: 'string'},
-      ],
-    }
   },
 }
 </script>
