43f25431b4182cb33172d9053251b41f0d116f6c
hemlk <hiep@elitetech.vn>
2017-12-15 12:32:26 +0700

    fix white bg when the Client detail popup is too long

--- src/components/client/ClientDetail.vue
+++ src/components/client/ClientDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-modal :no-backdrop-dismiss="isProcessing" :no-esc-dismiss="isProcessing" v-model="$store.state.isDetailShown" ref="popDetail" :content-css="{minWidth: '70vw', minHeight: '70vh'}">
+  <q-modal content-classes="bg-grey-9" :no-backdrop-dismiss="isProcessing" :no-esc-dismiss="isProcessing" v-model="$store.state.isDetailShown" ref="popDetail" :content-css="{minWidth: '70vw', minHeight: '70vh'}">
     <q-modal-layout>
       <q-toolbar slot="header">
         <q-btn color="warning" @click="discardClientChange" :disabled="isProcessing">
@@ -15,7 +15,7 @@
           Client Detail
         </q-toolbar-title>
       </q-toolbar>
-      <div class="bg-grey-9" style="padding: 15px">
+      <div style="padding: 15px">
         <q-field :label-width="3" icon="wifi" label="Client Code" :count="255" helper="Ma khach hang" error-label="Some error">
           <q-input v-model="getSelectedClient.code" dark color="yellow" />
         </q-field>
